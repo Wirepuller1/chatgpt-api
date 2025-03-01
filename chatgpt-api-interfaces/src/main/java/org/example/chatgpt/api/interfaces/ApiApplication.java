2a79cb386db4e7690ce0d25c8e1ecfacb51a7f3f
Wirepuller1 <3157979366@qq.com>
2025-03-01 22:19:53 +0800

    feat: init ddd project

--- chatgpt-api-interfaces/src/main/java/org/example/chatgpt/api/interfaces/ApiApplication.java
+++ chatgpt-api-interfaces/src/main/java/org/example/chatgpt/api/interfaces/ApiApplication.java
@@ -3,6 +3,11 @@ package org.example.chatgpt.api.interfaces;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 李伯阳
+ * @description 启动入口
+ * @github https://github.com/Wirepuller1/chatgpt-api
+ */
 @SpringBootApplication
 public class ApiApplication {
 
