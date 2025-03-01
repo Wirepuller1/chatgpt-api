e8b5162d507c9bb095df11a036c898848c618e70
Wirepuller1 <3157979366@qq.com>
2025-03-01 22:22:53 +0800

    feat: init ddd project

--- chatgpt-api-interfaces/src/test/java/org/example/chatgpt/api/interfaces/test/ApiTest.java
+++ chatgpt-api-interfaces/src/test/java/org/example/chatgpt/api/interfaces/test/ApiTest.java
@@ -1,4 +1,11 @@
 package org.example.chatgpt.api.interfaces.test;
 
+
+/**
+ * @author 李伯阳
+ * @description 单元测试
+ * @github https://github.com/Wirepuller1/chatgpt-api
+ */
+
 public class ApiTest {
 }
